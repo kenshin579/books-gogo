52ea052f85d290df9b54168f13f60483747d8a3b
kenshin579 <kenshin579@hotmail.com>
2020-12-19 23:35:05 +0900

    [BKG-9] array 슬라이스로 선언 추가

--- examples/array/array_test.go
+++ examples/array/array_test.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func Example_array() {
 	//fruits := [3]string{"사과", "바나나", "토마토"}
-	fruits := [...]string{"사과", "바나나", "토마토"}
+	//fruits := [...]string{"사과", "바나나", "토마토"}
+	fruits := []string{"사과", "바나나", "토마토"} //slice를 이용함
 	for _, fruit := range fruits {
 		fmt.Printf("%s는 맛있다.\n", fruit)
 	}
