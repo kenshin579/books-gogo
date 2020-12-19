eb37490b2cf39c218c6cd0993fa1e515f775c3e3
kenshin579 <kenshin579@hotmail.com>
2020-12-19 23:22:20 +0900

    [BKG-9] 3.2.1 array 스터디 완료

--- examples/array/array_test.go
+++ examples/array/array_test.go
@@ -2,8 +2,9 @@ package array
 
 import "fmt"
 
-func Example() {
-	fruits := [3]string{"사과", "바나나", "토마토"}
+func Example_array() {
+	//fruits := [3]string{"사과", "바나나", "토마토"}
+	fruits := [...]string{"사과", "바나나", "토마토"}
 	for _, fruit := range fruits {
 		fmt.Printf("%s는 맛있다.\n", fruit)
 	}
