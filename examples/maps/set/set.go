7a8605a58e506db53e967ac832e4addb1b60ed7f
kenshin579 <kenshin579@hotmail.com>
2020-12-22 18:31:08 +0900

    [BKG-9] 집합 (set)은 map을 사용함

--- examples/maps/set/set.go
+++ examples/maps/set/set.go
@@ -1,11 +1,32 @@
 package set
 
-func hasDupeRune(s string) bool {
-	runeSet := map[rune]struct{}{}
+import log "github.com/sirupsen/logrus"
+
+func hasDupeRune1(s string) bool {
+	runeSet := map[rune]bool{}
+
 	for _, r := range s {
-		if _, exists := runeSet[r]; exists {
+		log.Info("runeSet", runeSet)
+		if runeSet[r] {
 			return true
 		}
+		runeSet[r] = true
+	}
+	return false
+}
+
+func hasDupeRune2(s string) bool {
+	runeSet := map[rune]struct{}{}
+	for _, r := range s {
+		log.Info("runeSet", runeSet)
+		if _, exists := runeSet[r]; exists {
+			log.WithFields(log.Fields{
+				"runeSet[r]": runeSet[r],
+				"exists":     exists,
+			}).Info("test")
+			return true
+		}
+
 		runeSet[r] = struct{}{}
 	}
 	return false
--- examples/maps/set/set_test.go
+++ examples/maps/set/set_test.go
@@ -2,9 +2,17 @@ package set
 
 import "fmt"
 
-func ExampleHasDupeRune() {
-	fmt.Println(hasDupeRune("숨바꼭질"))
-	fmt.Println(hasDupeRune("다시합시다"))
+func ExampleHasDupeRune1() {
+	fmt.Println(hasDupeRune1("숨바꼭질"))
+	fmt.Println(hasDupeRune1("다시합시다"))
+	// Output:
+	// false
+	// true
+}
+
+func ExampleHasDupeRune2() {
+	fmt.Println(hasDupeRune2("숨바꼭질"))
+	fmt.Println(hasDupeRune2("다시합시다"))
 	// Output:
 	// false
 	// true
