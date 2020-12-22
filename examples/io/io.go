a826437d2c316c585998a83001ba490967c28354
kenshin579 <kenshin579@hotmail.com>
2020-12-22 20:14:44 +0900

    [BKG-9] 파일 읽기 쓰기

--- examples/io/io.go
+++ examples/io/io.go
@@ -18,6 +18,7 @@ func WriteTo(w io.Writer, lines []string) error {
 func ReadFrom(r io.Reader, lines *[]string) error {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
+		//todo : 정확하게 pointer를 사용해야 하는 게 언제 인가?
 		*lines = append(*lines, scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
