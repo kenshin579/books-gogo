266e3571bd96d161b826ff18ffc9dedcdfd55a00
kenshin579 <kenshin579@hotmail.com>
2020-12-27 22:01:49 +0900

    [BKG-15] NewMultiSet init

---
+++ exercises/chap3/multiset/multiset.go
@@ -0,0 +1,28 @@
+package multiset
+
+// 새로운 MultiSet을 생성하여 반환한다.
+func NewMultiSet() map[string]int {
+	return nil
+}
+
+// Insert 함수는 집합에 val을 추가한다.
+func Insert(m map[string]int, val string) {
+
+}
+
+// Erase 함수는 집합에서 val을 제거한다. 집합에 val이 없는
+// 경우에는 아무 일도 일어나지 않는다.
+func Erase(m map[string]int, val string) {
+
+}
+
+// Count 함수는 집합에 val이 들어 있는 횟수를 구한다.
+func Count(m map[string]int, val string) int {
+	return 0
+}
+
+// String 함수는 집합에 들어 있는 원소들을 { } 안에 빈 칸으로
+// 구분하여 넣은 문자열을 반환한다.
+func String(m map[string]int) string {
+	return ""
+}
---
+++ exercises/chap3/multiset/multiset_test.go
@@ -0,0 +1,30 @@
+package multiset
+
+import "fmt"
+
+func Example_MultiSet() {
+	m := NewMultiSet()
+
+	fmt.Println(String(m))
+	fmt.Println(Count(m, "3"))
+
+	Insert(m, "3")
+	Insert(m, "3")
+	Insert(m, "3")
+	Insert(m, "3")
+	fmt.Println(String(m))
+	fmt.Println(Count(m, "3"))
+
+	Insert(m, "1")
+	Insert(m, "2")
+	Insert(m, "5")
+	Insert(m, "7")
+	Erase(m, "3")
+	Erase(m, "5")
+	fmt.Println(Count(m, "3"))
+	fmt.Println(Count(m, "1"))
+	fmt.Println(Count(m, "2"))
+	fmt.Println(Count(m, "5"))
+	//Output:
+
+}
