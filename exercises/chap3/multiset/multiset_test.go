0e785a61b9c873d3d1c3473423510c635f9cbfe5
kenshin579 <kenshin579@hotmail.com>
2020-12-27 22:27:21 +0900

    [BKG-15] NewMultiSet 완료

--- exercises/chap3/multiset/multiset_test.go
+++ exercises/chap3/multiset/multiset_test.go
@@ -6,14 +6,14 @@ func Example_MultiSet() {
 	m := NewMultiSet()
 
 	fmt.Println(String(m))
-	fmt.Println(Count(m, "3"))
+	fmt.Println(Count(m, "3")) //0
 
 	Insert(m, "3")
 	Insert(m, "3")
 	Insert(m, "3")
 	Insert(m, "3")
 	fmt.Println(String(m))
-	fmt.Println(Count(m, "3"))
+	fmt.Println(Count(m, "3")) //4
 
 	Insert(m, "1")
 	Insert(m, "2")
@@ -25,6 +25,14 @@ func Example_MultiSet() {
 	fmt.Println(Count(m, "1"))
 	fmt.Println(Count(m, "2"))
 	fmt.Println(Count(m, "5"))
-	//Output:
 
+	//Output:
+	//{ }
+	//0
+	//{ 3 3 3 3 }
+	//4
+	//3
+	//1
+	//1
+	//0
 }
