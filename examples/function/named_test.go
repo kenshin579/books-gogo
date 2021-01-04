0e243af9a2559dfdb24ac785a658e779713dc2e9
kenshin579 <kenshin579@hotmail.com>
2021-01-04 12:50:19 +0900

    [BKG-5] 명명된 함수에 대한 예제 정리

--- examples/function/named_test.go
+++ examples/function/named_test.go
@@ -17,10 +17,30 @@ func ExampleBinOpToBinSub() {
 	// count: 3
 }
 
-func Example_BinOp() {
-	OpThreeAndFour(func(a, b int) int {
+func Example_BinOp_임명_함수라_형변환이_자동으로_일어난다() {
+	OpThreeAndFour(func(a, b int) int { //임명함수 (함수 리터럴)
 		return 1 + 3
 	})
 	// Output:
 	//4
+}
+
+func Example_BinOp_자료형_검사를_한다() {
+	binOp := BinOp(func(a, b int) int {
+		return 1 + 3
+	})
+
+	OpThreeAndFour(binOp)
+	// Output:
+	//4
+}
+
+func Example_BinSub_자료형_검사를_한다2() {
+	binSub := BinSub(func(a, b int) int {
+		return a + b
+	})
+	//OpThreeAndFour(binSub) //Cannot use 'binSub' (type BinSub) as type BinOp
+	fmt.Println(binSub)
+
+	// Output:
 }
