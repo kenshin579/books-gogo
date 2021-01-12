49fbd474eaa451dd5b9c67e65e3ff236bc1e72bc
kenshin579 <kenshin579@hotmail.com>
2021-01-12 22:08:40 +0900

    [BKG-6] 코멘트 추가

--- examples/slice/slice_test.go
+++ examples/slice/slice_test.go
@@ -20,7 +20,7 @@ func Example_slicing() {
 func Example_append() {
 	f1 := []string{"사과", "바나나", "토마토"}
 	f2 := []string{"포도", "딸기"}
-	f3 := append(f1, f2...)     // 이어붙이기
+	f3 := append(f1, f2...)     // 이어붙이기 (가변 인자)
 	f4 := append(f1[:2], f2...) // 토마토를 제외하고 이어붙이기
 
 	fmt.Println(f1)
