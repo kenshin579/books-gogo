d320d90f9e6916385d9d0bb1f6ff5713a396b0f1
kenshin579 <kenshin579@hotmail.com>
2021-01-14 22:13:23 +0900

    [BKG-6] 외부 의존성 줄이기 예제 추가

--- interface/filesystem/filesystem.go
+++ interface/filesystem/filesystem.go
@@ -1,6 +1,9 @@
 package filesystem
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 type FileSystem interface {
 	Rename(oldPath, newpath string) error
@@ -10,14 +13,26 @@ type FileSystem interface {
 type OSFileSystem struct{}
 
 func (fs OSFileSystem) Rename(oldpath, newpath string) error {
+	fmt.Printf("rename %s -> %s\n", oldpath, newpath)
+
 	return os.Rename(oldpath, newpath)
 }
 
 func (fs OSFileSystem) Remove(name string) error {
+	fmt.Printf("remove %s\n", name)
 	return os.Remove(name)
 }
 
 //인터페이스를 사용함
 func ManageFiles(fs FileSystem) {
 	//todo : 이건 어떻게 사용할 있나?
+	fmt.Println("ManagedFiles called")
+	err := fs.Rename("oldpath", "newpath")
+	if err != nil {
+		fmt.Errorf("error while calling rename: %w", err)
+	}
+	err = fs.Remove("file")
+	if err != nil {
+		fmt.Errorf("error while calling remove: %w", err)
+	}
 }
--- interface/filesystem/filesystem_test.go
+++ interface/filesystem/filesystem_test.go
@@ -1,6 +1,10 @@
 package filesystem
 
-//todo : 이걸 정확하게 어떻게 작성을 해야 하나?
-func ExampleManageFiles() {
+import "testing"
 
+//todo : 이걸 정확하게 어떻게 작성을 해야 하나?
+
+func Test(t *testing.T) {
+	var fileSystem FileSystem = OSFileSystem{}
+	ManageFiles(fileSystem)
 }
