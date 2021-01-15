5b66a7a26da20a67a7cfffc6903074fbcd760d81
kenshin579 <kenshin579@hotmail.com>
2021-01-15 13:53:34 +0900

    [BKG-7] 설명 추가

--- net/http/fileserver/fileserver.go
+++ net/http/fileserver/fileserver.go
@@ -12,8 +12,9 @@ var (
 	root = flag.String("root", "/var/www", "root directory")
 )
 
+//go run fileserver.go --addr=localhost:8000 --root=/home/me/www
 func main() {
-	flag.Parse()
+	flag.Parse() //command option
 	log.Fatal(http.ListenAndServe(
 		*addr,
 		http.FileServer(http.Dir(*root)),
