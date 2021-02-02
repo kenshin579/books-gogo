3a38e4e4fe445151dfc311e21622064f05106e16
kenshin579 <kenshin579@hotmail.com>
2021-02-02 23:27:45 +0900

    [BKG-9] 상속 관련 스터디

--- examples/polymorph/polymorph.go
+++ examples/polymorph/polymorph.go
@@ -16,6 +16,7 @@ type Rectangle struct {
 	Width, Height float32
 }
 
+//메서드 추가
 func (r Rectangle) Area() float32 {
 	return r.Width * r.Height
 }
@@ -36,8 +37,10 @@ func TotalArea(shapes []Shape) float32 {
 	return total
 }
 
+//구조체에 다른 구조체를 넣어서 필드 참조나 메서드 호출을 위해 불필요하 코드 작성하는 것을 피할 수 있음
 type RectangleCircum struct{ Rectangle }
 
+//메서드 추가
 func (r RectangleCircum) Circum() float32 {
 	return 2 * (r.Width + r.Height)
 }
@@ -46,6 +49,7 @@ func NewRectangle(width, height float32) *RectangleCircum {
 	return &RectangleCircum{Rectangle{width, height}}
 }
 
+//오버라이딩
 type WrongRectangle struct{ Rectangle }
 
 func (r WrongRectangle) Area() float32 {
--- examples/polymorph/polymorph_test.go
+++ examples/polymorph/polymorph_test.go
@@ -1,6 +1,9 @@
 package polymorph
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func ExampleTotalArea() {
 	fmt.Println(TotalArea([]Shape{
@@ -35,4 +38,22 @@ func ExampleTotalArea_moreTypes() {
 		WrongRectangle{Rectangle{1, 2}},
 	}))
 	// Output: 126
+}
+
+//내장된 구조체가 있는지는 구조체에서 내장된 구조체의 이름으로 필드를 찾은 다음에 anonymous 필드를 찾아보면 된다
+func Example_ReflectTypeOf로_어떤_자료형이_주어진_인터페이스를_구현하고_있는지_여부_확인() {
+	//impl := reflect.TypeOf(RectangleCircum{}).Implements(
+	//	reflect.TypeOf((*Shape)(nil)).Elem(),
+	//	)
+
+	//RectangleCircum{} 빈 객체를 만들지 않고 하는 방법
+	impl := reflect.TypeOf((*RectangleCircum)(nil)).Elem().Implements(reflect.TypeOf((*Shape)(nil)).Elem())
+	fmt.Println(impl)
+
+	field, ok := reflect.TypeOf(RectangleCircum{}).FieldByName("Rectangle")
+	emb := ok && field.Anonymous && field.Type == reflect.TypeOf(Rectangle{})
+	fmt.Println(emb)
+	//Output:
+	//true
+	//true
 }
